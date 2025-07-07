22e0a2fd74e1dc0a912c0d86ff25caba4ce49d8b
Karthik Sivakumar <karthik29062000@gmail.com>
2025-07-07 11:24:02 +0530

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import { Providers } from "@/app/providers";
 import { Toaster } from "@/components/ui/sonner";
@@ -36,6 +37,7 @@ export default function RootLayout({
                         <Footer />
                     </div>
                 </Providers>
+                <Analytics />
             </body>
         </html>
     );
